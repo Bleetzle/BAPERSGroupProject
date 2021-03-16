27e35da8e34febedbd91f2155d28b07115b962f6
Bleetzle <60585266+Bleetzle@users.noreply.github.com>
2021-03-16 01:48:40 +0000

    Refactored gui into folders, created all shiftmanager and reports gui

---
+++ Bapers/Bapers/GUI/reports/viewReport.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bapers.GUI.reports
+{
+    /// <summary>
+    /// Interaction logic for viewReport.xaml
+    /// </summary>
+    public partial class viewReport : Window
+    {
+        public viewReport()
+        {
+            InitializeComponent();
+        }
+
+        private void printReport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
+            reportPortal reportPortalWindow = new reportPortal();
+            reportPortalWindow.Show();
+            this.Close();
+        }
+
+        private void logOut_Click(object sender, RoutedEventArgs e)
+        {
+            Login loginWindow = new Login();
+            loginWindow.Show();
+            this.Close();
+        }
+    }
+}
---
+++ Bapers/Bapers/GUI/shiftManager/shiftManager.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bapers.GUI.shiftManager
+{
+    /// <summary>
+    /// Interaction logic for shiftManager.xaml
+    /// </summary>
+    public partial class shiftManager : Window
+    {
+        public shiftManager()
+        {
+            InitializeComponent();
+        }
+
+        private void reception_click(object sender, RoutedEventArgs e)
+        {
+            receptionist receptionistWindow = new receptionist();
+            receptionistWindow.Show();
+            this.Close();
+        }
+
+        private void report_click(object sender, RoutedEventArgs e)
+        {
+            reports.reportPortal reportPortalWindow = new reports.reportPortal();
+            reportPortalWindow.Show();
+            this.Close();
+           
+        }
+
+        private void logOut_Click(object sender, RoutedEventArgs e)
+        {
+            Login loginWindow = new Login();
+            loginWindow.Show();
+            this.Close();
+        }
+    }
+}
