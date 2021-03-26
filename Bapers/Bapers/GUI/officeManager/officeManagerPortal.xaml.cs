33f2bf7db09354e405d27192e90f2a348f329d7c
Bleetzle <60585266+Bleetzle@users.noreply.github.com>
2021-03-26 00:05:10 +0000

    added deadline close to notification

--- Bapers/Bapers/GUI/officeManager/officeManagerPortal.xaml.cs
+++ Bapers/Bapers/GUI/officeManager/officeManagerPortal.xaml.cs
@@ -25,6 +25,26 @@ namespace Bapers.GUI.officeManager
         {
             myVariables.myStack.Push("Office Manager");
             InitializeComponent();
+            deadlineChecker();
+        }
+
+        private async void deadlineChecker()
+        {
+            bool deadline = await db.Check("select job_Number, deadline, job_status from job where job_status = 'Uncompleted' AND deadline between curdate() AND date_format(curdate() + 4, '%Y-%m-%d')");
+            if (deadline)
+            {
+                var list1 = new List<string>();
+                var list2 = new List<string>();
+                await db.SelectLists(list1, "select job_Number from job where job_status = 'Uncompleted' AND deadline between curdate() AND date_format(curdate() + 4, '%Y-%m-%d')");
+                await db.SelectLists(list2, "select deadline from job where job_status = 'Uncompleted' AND deadline between curdate() AND date_format(curdate() + 4, '%Y-%m-%d')");
+                string notification= "Deadline coming soon for \n";
+               for(int i = 0; i < list1.Count(); i++)
+                {
+                    notification = notification + list1[i] + "   Due at: " + list2[i] + "\n";
+                }
+                System.Windows.Forms.MessageBox.Show(notification);
+            }
+            
         }
 
         private void create_click(object sender, RoutedEventArgs e)
